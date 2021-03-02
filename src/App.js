52e8f538e6f5b03e328bdeea59b239acb933f684
Rebecca Blanton <beccaablanton@gmail.com>
2021-03-01 18:31:27 -0800

    updated app.js, added initial axios and API for random user generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from "react";
+import Table from "./Table";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://randomuser.me/api/");
+      setData(result.data);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
